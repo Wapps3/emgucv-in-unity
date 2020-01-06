815ba620e52431771e7c4008cf4b84b71258c6a5
Duvignau Thomas <thomaslaforce@yahoo.fr>
2020-01-06 15:50:13 +0100

    First commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Emgu.CV; // the big one
+using Emgu.CV.Util; // Vectors
+using Emgu.CV.CvEnum; //Utility for constants
+using Emgu.CV.Structure;
+
+using System;
+using System.Drawing; //Point
+using System.IO; //MemoryStream, use to convert cv::Mat to Texture2D
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
