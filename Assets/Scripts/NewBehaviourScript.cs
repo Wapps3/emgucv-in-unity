bc9376cce930746fd754918026ff2f1fa47b67ea
Duvignau Thomas <thomaslaforce@yahoo.fr>
2020-01-08 10:13:00 +0100

    Add Hsv threshold

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -10,17 +10,31 @@ using Emgu.CV.Structure;
 using System;
 using System.Drawing; //Point
 using System.IO; //MemoryStream, use to convert cv::Mat to Texture2D
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
     VideoCapture video;
+
+    [Range(0, 179)]
+    public int cMin;
+    [Range(0, 179)]
+    public int cMax = 179;
+    [Range(0, 255)]
+    public int sMin;
+    [Range(0, 255)]
+    public int sMax = 255;
+    [Range(0, 255)]
+    public int vMin;
+    [Range(0, 255)]
+    public int vMax = 255;
     
     //VideoCapture webcam;
 
     // Start is called before the first frame update
     void Start()
     {
-        string path = "D:\\M2\\Interface\\emgucv-in-unity\\Assets\\Video\\2019-10-29 17-34-21.mp4";
+        //string path = "D:\\M2\\Interface\\emgucv-in-unity\\Assets\\Video\\2019-10-29 17-34-21.mp4";
         video = new VideoCapture(0);   
 
         //video = new VideoCapture(0); //First webcam start at 0
@@ -29,14 +43,12 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //CvInvoke.CvtColor(image, imgGray, ColorConversion.Bgr2Gray);
+
         Mat image;
 
         //Query the frame
         image = video.QueryFrame();
-
-        //Gray img
-        Mat imgGray = image.Clone();
-        CvInvoke.CvtColor(image,imgGray,ColorConversion.Bgr2Gray);
 
         //HSV img
         Mat imgHSV = image.Clone();
@@ -44,23 +56,18 @@ public class NewBehaviourScript : MonoBehaviour
 
         //Blur
         Mat imgHSVBlur = imgHSV.Clone();
-        CvInvoke.GaussianBlur(imgHSVBlur, imgHSVBlur, new Size(21,21) ,0,0, BorderType.Reflect101);
+        CvInvoke.MedianBlur(imgHSVBlur, imgHSVBlur, 15);
 
         //New Img
-        Image<Bgr, Byte> newImg = image.ToImage<Bgr, Byte>();
+        Image<Hsv, Byte> newImg = imgHSVBlur.ToImage<Hsv, Byte>();
 
-        Hsv lowerBound = new Hsv(60,0,0);
-        Hsv upperBound = new Hsv(180,255,255);
+        Hsv lowerBound = new Hsv(cMin,sMin,vMin);
+        Hsv higherBound = new Hsv(cMax,sMax,vMax);
 
-        newImg.inRange(lowerBound, upperBound);
-
-       
+        Image<Gray,Byte> thresholdImg = newImg.InRange(lowerBound, higherBound);
 
         //Invoke C++ interface fonction "Imshow"
         CvInvoke.Imshow("Video view", image);
-
-        //Invoke C++ interface fonction "Imshow"
-        CvInvoke.Imshow("Video view gray", imgGray);
 
         //Invoke C++ interface fonction "Imshow"
         CvInvoke.Imshow("Video view hsv", imgHSV);
@@ -69,7 +76,7 @@ public class NewBehaviourScript : MonoBehaviour
         CvInvoke.Imshow("Video view hsv with blur", imgHSVBlur);
 
         // Invoke C++ interface fonction "Imshow"
-        CvInvoke.Imshow("Video view image format", newImg);
+        CvInvoke.Imshow("Video view image seuillage", thresholdImg);
 
         //Block thread for 24 milisecond
         CvInvoke.WaitKey(24);
